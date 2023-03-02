8d3cd6489db907a8427d1c2f488a3152a1b4b601
alice <zhupanova2017sv@mail.ru>
2023-03-02 20:51:15 +0300

    numbers of interval N to 1

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+System.Console.WriteLine("Задайте значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(Interval(N, 1));
+
+string Interval(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return start + "," + " " + Interval(start - 1, end);
+}
